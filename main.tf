804826ea47201c147124b502cedfb3cc61db6e31
TeenaKohli <teena.kohli.8622@gmail.com>
2021-12-11 06:06:11 +0000

    terraform changes

--- main.tf
+++ main.tf
@@ -16,11 +16,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0ed9277fb7eb570c9"
   instance_type = "t2.micro"
-<<<<<<< HEAD
   user_data     = file("file.sh")
-=======
-  user_data     = "file.sh"
->>>>>>> 879cfe0f7b0cbcc14e9d296b6315372ea32761bc
 
   tags = {
     Name = "ExampleAppServerInstance"
