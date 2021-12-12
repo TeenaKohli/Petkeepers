d6881ec9a848a06816721e22d09d8c69daf88182
pratikkasalkar <pratikkasalkar@gmail.com>
2021-12-12 17:20:13 +0000

    Update PetkeepersApplicationTests.java
    added test cases

--- src/test/java/com/nci/petkeepers/PetkeepersApplicationTests.java
+++ src/test/java/com/nci/petkeepers/PetkeepersApplicationTests.java
@@ -1,27 +1,35 @@
 package com.nci.petkeepers;
 
+import com.nci.petkeepers.model.Users;
+import com.nci.petkeepers.repository.UsersRepository;
+import org.apache.catalina.User;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.scheduling.annotation.SchedulingConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+@SpringBootTest
 class PetkeepersApplicationTests {
 
     @Autowired
-    private MockMvc mvc;
+    UsersRepository usersRepository;
 
+
+    @Test
+    void contextLoads() {
+
+    }
+
+    @Test
+    void checkDB(){
+        usersRepository.count();
+        Assert.assertEquals(usersRepository.count(),4);
+    }
+
+    @Test
+    void checkDBname(){
+        Users users = usersRepository.findByUsername("pratikkasalkar");
+        Assert.assertEquals("pratikkasalkar", users.getUsername());
+    }
 
 }
